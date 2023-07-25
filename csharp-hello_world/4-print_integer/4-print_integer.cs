62c3817860a584865ebd15046f6764152dd99949
Jagucan <jjgc.2499@gmail.com>
2023-07-25 02:21:56 -0500

    Print integer

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+        Console.WriteLine(number + " " + "Mission Street");
+	}
+}
