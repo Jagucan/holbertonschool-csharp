cab01bbe10190ce072a455663fca2bcb0096b15e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 02:36:15 -0500

    concat string

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -5,8 +5,7 @@ namespace _7_concat {
         static void Main(string[] args) {
             string str1 = "Holberton";
 		    string str2 = "School";
-		    str1 += " " + str2;
-		    Console.WriteLine("Welcome to {0}!", str1);
+		    Console.WriteLine("Welcome to {0} {1}!", str1, str2);
         }
     }
 }
