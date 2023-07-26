9cf5041d9e16c9a138301a794fad694c37bbe6cb
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 16:33:16 -0500

    comvert to hexa

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseHex = 16;
+            for (int num = 0; num <= 98; num++)
+            {
+                string hex = Convert.ToString(num, baseHex);
+                Console.Write("{0} = 0x{1}\n", num, hex);
+            }
+        }
+    }
+}
