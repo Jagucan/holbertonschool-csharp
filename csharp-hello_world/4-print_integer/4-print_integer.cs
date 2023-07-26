a4f43cf559bbee1930f4e7ebfe5b10d7d560591f
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 02:10:54 -0500

    edit task 4

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -4,7 +4,7 @@ namespace _4_print_integer {
     class Program {
         static void Main(string[] args) {
             int number = 972;
-            Console.WriteLine(number);
+            Console.WriteLine(number + "Mission Street");
         }
     }
 }
