eefe501d90d0b7b6babfcb3a79835467c88e9396
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 02:30:32 -0500

    Print string

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
-namespace _6_print_string
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace _6_print_string {
+    class Program {
+        static void Main(string[] args) {
             string str = "Holberton School";
+            string separator = " ";
+            string[] tokens = str.Split(separator);
+            Console.Write(str);
+            Console.Write(str);
             Console.WriteLine(str);
+            Console.WriteLine(String.Join(Environment.NewLine, tokens[0]));
         }
     }
 }
