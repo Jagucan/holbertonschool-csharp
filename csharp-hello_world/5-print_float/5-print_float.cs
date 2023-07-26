07854f9e1236f32a4696f2a348ce1c176b0d17f0
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 02:13:13 -0500

    edit task 5

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace _5_print_float
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace _5_print_float {
+    class Program {
+        static void Main(string[] args) {
             float number = 3.14159f;
             Console.WriteLine("{0:N2}", number);
         }
