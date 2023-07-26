fc6a4fde106fe3d1a1087909ab9d5b4cb332321e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 02:09:31 -0500

    edit task 4

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace _4_print_integer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace _4_print_integer {
+    class Program {
+        static void Main(string[] args) {
             int number = 972;
             Console.WriteLine(number);
         }
