0ed2c398e150771bf8206a6a5211a997db3ec51a
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 17:01:25 -0500

    prints numbers from 0 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _4_print_hexa {
+    class Program {
+        static void Main(string[] args) {
+            for (int num = 0; num <= 99; num++) {
+                if (num < 10) {
+                    Console.Write("0{0},", num);
+                }
+                else if (num == 99) {
+                    Console.Write("{0}", num);
+                }
+                else {
+                    Console.Write("{0},", num);
+                }
+            }
+        }
+    }
+}
