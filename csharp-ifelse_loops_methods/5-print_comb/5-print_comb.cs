82892ec89ac1de1d64a33e35e6f9a908e88006b2
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-26 18:38:07 -0500

    change format to D2 and more

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -3,14 +3,12 @@
 namespace _4_print_hexa {
     class Program {
         static void Main(string[] args) {
+            string[] numbers = new string[100];
             for (int num = 0; num <= 99; num++) {
-                if (num < 99) {
-                    Console.Write("{0:D2}, ", num);
-                }
-                else {
-                    Console.Write("{0}", num);
-                }
+                numbers[num] = num.ToString("D2");
             }
+            string result = string.Join(", ", numbers);
+            Console.Write(result);
         }
     }
 }
