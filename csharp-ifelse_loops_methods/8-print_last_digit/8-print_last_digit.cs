2beb77b1c4d94f72082fa6ed5776383ef5725515
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-27 13:24:52 -0500

    prints the last digit of a number.

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int positiveNum = 0;
+            int lastDigit = number % 10;
+
+            if (number < 0)
+            {
+                positiveNum = lastDigit * -1;
+                Console.Write(positiveNum);
+                return positiveNum;
+            }
+            else
+            {
+                Console.Write(lastDigit);
+                return lastDigit;
+            }
+        }
+    }
+}
+
