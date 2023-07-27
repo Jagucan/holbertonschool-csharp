063a73b471a6a10a678efd148bf9448f782381ca
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-27 11:22:34 -0500

    edit task 6

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -2,27 +2,31 @@
 
 namespace _6_print_comb2
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int nums = 0;
-			for (int firstDigit = 0; firstDigit <= 9; firstDigit++)
-			{
-				for (int secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
-				{
-					nums = firstDigit * 10 + secondDigit;
-					if (nums < 89)
-					{
-						Console.Write("{0:D2}, ", nums);
-					}
-					else
-					{
-						Console.Write("{0:D2}", nums);
-						break;
-					}
-				}
-			}
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 0;
+            string output = "";
+
+            for (int firstDigit = 0; firstDigit <= 9; firstDigit++)
+            {
+                for (int secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
+                {
+                    nums = firstDigit * 10 + secondDigit;
+                    if (nums < 89)
+                    {
+                        output += $"{nums:D2}, ";
+                    }
+                    else
+                    {
+                        output += $"{nums:D2}";
+                        break;
+                    }
+                }
+            }
+
+            Console.Write(output);
+        }
+    }
 }
