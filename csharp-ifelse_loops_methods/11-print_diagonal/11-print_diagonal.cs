457f1076d95f24a6c3ffa67678ae5cc6117bff33
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-07-27 14:01:23 -0500

    draws a diagonal line

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            for (int item = 0; item < length; item++)
+            {
+                for (int space = 0; space < item; space++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
