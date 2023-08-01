9ad30c6ab1f6dfede6a10d62ee49d7585bfc2756
Jagucan <jjgc.2499@gmail.com>
2023-08-01 16:29:13 -0500

    reverse array

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Array
+{
+	public static void Reverse(int[] array)
+	{
+		if (array == null)
+		{
+			Console.WriteLine();
+			return;
+		}
+		
+		int[] reverse = Enumerable.Reverse(array).ToArray();
+		
+		if (reverse.Length >= array.Length - 1)
+		{
+			Console.WriteLine(String.Join(" ", reverse));
+		}
+		else
+		{
+			Console.WriteLine(String.Join("", reverse));  
+		}	
+	}
+}
