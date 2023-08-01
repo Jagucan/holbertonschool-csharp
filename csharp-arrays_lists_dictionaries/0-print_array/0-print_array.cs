3519d79dcbd383a9f5f779117856b2acee411b6d
Jagucan <jjgc.2499@gmail.com>
2023-07-31 19:59:23 -0500

    task 0

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,32 +2,32 @@
 
 namespace _0_print_array
 {
-    public class Array
-    {
-        public static int[] CreatePrint(int size)
-        {
-            if (size == 0)
-            {
-                Console.WriteLine();
-                return new int[0];
-            }
-            else if (size < 0)
-            {
-                Console.Write("Size cannot be negative");
-                return null;
-            }
-            else
-            {
-                int[] newArray = new int[size];
+	class Array
+	{
+		public static int[] CreatePrint(int size)
+		{
+			if (size == 0)
+			{
+				Console.WriteLine();
+				return new int[0];
+			}
+			else if (size < 0)
+			{
+				Console.Write("Size cannot be negative");
+				return null;
+			}
+			else
+			{
+				int[] myArray = new int[size];
 
-                for (int num = 0; num < size; num++)
-                {
-                    newArray[num] = num;
-                    Console.Write("{0} ", newArray[num]);
-                }
-                Console.WriteLine();
-                return newArray;
-            }
-        }
-    }
+				for (int num = 0; num < size; num++)
+				{
+					myArray[num] = num;
+					Console.Write("{0} ", myArray[num]);
+				}
+				Console.WriteLine();
+				return myArray;
+			}
+		}
+	}
 }
