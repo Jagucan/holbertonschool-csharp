0c3819e1f1a5d5e1f0094f2d6da55866b12ee643
Jagucan <jjgc.2499@gmail.com>
2023-08-01 15:25:29 -0500

    edit task 2

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,12 +4,12 @@ class Array
 {
 	public static int[] ReplaceElement(int[] array, int index, int n)
 	{
-		if (index > array.Length || index < 0 || array.Length <= 0)
+		if (index < array.Length && index > 0)
 		{
-			Console.WriteLine("Index out of range");
+			array[index] = n;
 			return array;
 		}
-		array[index] = n;
+		Console.WriteLine("Index out of range");
 		return array;
 	}
 }
