3dc77c1068c1f7eeb6fa6361fd4531b30c80632a
Jagucan <jjgc.2499@gmail.com>
2023-08-02 17:07:42 -0500

    delete item in a list

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static void DeleteAt(List<int> myList, int index)
+    {
+        if (index >= 0 && index < myList.Count)
+        {
+            List<int> newList = new List<int>();
+
+            for (int num = 0; num < myList.Count; num++)
+            {
+                if (num != index)
+                {
+                    newList.Add(myList[num]);
+                }
+            }
+
+            myList.Clear();
+            myList.AddRange(newList);
+        }
+        else
+        {
+            Console.WriteLine("Index is out of range");
+        }
+    }
+}
