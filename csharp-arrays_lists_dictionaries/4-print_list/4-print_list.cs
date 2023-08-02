06e4a44b7e5ef9e4022eb8e06d0bed2367ca96ea
Jagucan <jjgc.2499@gmail.com>
2023-08-01 19:00:57 -0500

    creates and prints a list

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size == 0)
+		{
+			Console.WriteLine();
+		}
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+		else
+		{
+			List<int> myList = new List<int>();
+
+			for (int num = 0; num < size; num++)
+			{
+				myList.Add(num);
+				if (num < size -1)
+				{
+					Console.Write("{0} ", num);
+				}
+				else
+				{
+					Console.WriteLine("{0}", num);
+				}
+			}
+			return myList;
+		}
+	}
+}
