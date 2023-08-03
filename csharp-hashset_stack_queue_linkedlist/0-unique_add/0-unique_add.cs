bf31aefd166121b1348955be87b3d31c0c47f58f
Jagucan <jjgc.2499@gmail.com>
2023-08-03 16:10:27 -0500

    adds all unique integers in a list.

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+        int sum = 0;
+        HashSet<int> uniqueNumbers = new HashSet<int>();
+
+        foreach (var num in myList)
+        {
+            if (!uniqueNumbers.Contains(num))
+            {
+                sum += num;
+                uniqueNumbers.Add(num);
+            }
+        }
+
+        return sum;
+	}
+}
