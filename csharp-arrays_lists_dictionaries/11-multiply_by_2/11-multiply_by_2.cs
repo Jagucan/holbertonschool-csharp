538602a0534a4f18c4001fa6ad395a3c82e4121a
Jagucan <jjgc.2499@gmail.com>
2023-08-02 19:42:57 -0500

    values multiplied by 2.

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+	{
+		Dictionary<string, int> newDict = new Dictionary<string, int>();
+		int factor = 2;
+
+		foreach (var data in myDict)
+		{
+			newDict[data.Key] = data.Value * factor;
+		}
+		return newDict;
+	}
+}
