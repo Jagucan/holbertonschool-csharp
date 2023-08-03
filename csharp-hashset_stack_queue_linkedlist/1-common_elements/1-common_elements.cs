e3d49c7c03dddef5fe34422b031c839376b229fb
Jagucan <jjgc.2499@gmail.com>
2023-08-03 18:19:51 -0500

    common elements in two lists.

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+		HashSet<int> commonNumbers = new HashSet<int>();
+        foreach (var num in list1)
+        {
+            if (list2.Contains(num))
+            {
+                commonNumbers.Add(num);
+            }
+        }
+
+        return new List<int>(commonNumbers);
+	}
+}
