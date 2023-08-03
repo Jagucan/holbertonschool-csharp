f92b9638537d0addd4934063d5dbf333bd83089b
Jagucan <jjgc.2499@gmail.com>
2023-08-03 14:11:04 -0500

    search the biggest value

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,25 +5,16 @@ class Dictionary
 {
 	public static string BestScore(Dictionary<string, int> myList)
 	{
-		string best = "";
-		int bestScore = 0;
-
-		if (myList == null || myList.Count < 0)
+		if (myList == null || myList.Count == 0)
 		{
-            return null;
-        }
-		else
-		{
-			foreach (KeyValuePair<string, int> pair in myList)
-			{
-				if (pair.Value > bestScore)
-				{
-					best = pair.Key;
-					bestScore = pair.Value;
-				}
-				
+			return "None";
+		}
+		KeyValuePair<string,int> max = new KeyValuePair<string,int>(); 
+		foreach (var entry in myList) {
+			if (entry.Value > max.Value) {
+				max = entry;
 			}
 		}
-		return best;
+		return max.Key;
 	}
 }
