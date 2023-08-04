02a0e2f28f826b53180f119ba1367070e66817ed
Jagucan <jjgc.2499@gmail.com>
2023-08-04 17:24:42 -0500

    Get node at index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int nodePosistion = 0;
+		foreach (int i in myLList)
+		{
+			nodePosistion++;
+			if (nodePosistion == n + 1)
+			{
+				return i;
+			}
+		}
+		return 0;
+	}
+}
