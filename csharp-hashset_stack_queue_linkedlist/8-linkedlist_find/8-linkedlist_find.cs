87b44dd728604e03bfc15692d9a4a06ea4dfe5f0
Jagucan <jjgc.2499@gmail.com>
2023-08-04 16:41:28 -0500

    Find value position

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        LinkedListNode<int> currentNode = myLList.First;
+		int nodePosition = 0;
+
+        while (currentNode != null)
+        {
+            if (currentNode.Value == value)
+            {
+                return nodePosition;
+            }
+
+            currentNode = currentNode.Next;
+			nodePosition++;
+        }
+
+        return -1;
+    }
+}
