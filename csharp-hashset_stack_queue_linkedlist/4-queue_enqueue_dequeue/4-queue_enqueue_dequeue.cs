6bc1cca21a268f861e4031a8a27649301beb09d4
Jagucan <jjgc.2499@gmail.com>
2023-08-03 20:26:25 -0500

    Queue it up

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		if (aQueue == null)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        else if (aQueue.Count == 0)
+		{
+			bool containsSearch = aQueue.Contains(search);
+			Console.WriteLine("Number of items: {0}", aQueue.Count);
+            Console.WriteLine("Stack is empty");
+			Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+			aQueue.Enqueue(newItem);
+        }
+
+		else
+        {
+            Console.WriteLine("Number of items: {0}", aQueue.Count);
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+
+            bool containsSearch = aQueue.Contains(search);
+            Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+
+            if (containsSearch)
+            {
+                Stack<string> tempStack = new Stack<string>();
+                string currentItem;
+
+                do
+                {
+                    currentItem = aQueue.Dequeue();
+                } while (currentItem != search && aQueue.Count > 0);
+            }
+
+            aQueue.Enqueue(newItem);
+        }
+
+        return aQueue;
+	}
+}
