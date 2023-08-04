476daffeada063b49128000b3744a3a0c6063b17
Jagucan <jjgc.2499@gmail.com>
2023-08-03 19:02:09 -0500

    Only differents

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+	{
+		HashSet<int> uniqueNumbers1 = new HashSet<int>(list1);
+        HashSet<int> uniqueNumbers2 = new HashSet<int>(list2);
+
+        uniqueNumbers1.SymmetricExceptWith(uniqueNumbers2);
+
+        List<int> result = new List<int>(uniqueNumbers1);
+        result.Sort();
+
+        return result;
+	}
+}
