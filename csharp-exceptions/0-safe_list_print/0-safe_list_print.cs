8efce8b060a50a64ead6ffad8f2d93e79304ee67
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-14 16:34:55 -0500

    Safe list printing

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int nums = 0;
+
+        for (int i = 0; i < myList.Count && nums < n; i++)
+        {
+            Console.WriteLine(myList[i]);
+            nums++;
+        }
+        
+        return nums;
+    }
+}
+
