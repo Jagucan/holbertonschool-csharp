27e53ba1b398eebf933bc274eca9dae4f4af5d6d
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-14 20:12:52 -0500

    override the .ToString() method

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog (string sName, float sAge, string sOwner, Rating sRating)
+    {
+        name = sName;
+        age = sAge;
+        owner = sOwner;
+        rating = sRating;
+    }
+    public override string ToString()
+    {
+        string formattedAge = age.ToString("0.0", CultureInfo.InvariantCulture);
+        return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}\n", name, formattedAge, owner, rating);
+    }
+}
+
