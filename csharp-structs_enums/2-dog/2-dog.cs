0bc10dbb877a3015c185b70ada6ff5217905964e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-14 19:52:09 -0500

    Create Constructor

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog (string sName, float sAge, string sOwner, Rating sRating)
+    {
+        name = sName;
+        age = sAge;
+        owner = sOwner;
+        rating = sRating;
+    }
+}
