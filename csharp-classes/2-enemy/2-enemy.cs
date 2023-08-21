b9bbb56b3f42cdd1904a98d124fc11bad830662c
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-21 10:15:49 -0500

    task 3

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class represents a Zombie type enemy.
+    /// </summary>
+    public class Zombie
+    {
+
+        public int health;
+
+        /// <summary>Initializes a new instance of the Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>Initializes a new instance of the Zombie class with an exception.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
