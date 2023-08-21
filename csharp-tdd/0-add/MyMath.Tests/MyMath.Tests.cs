1557257c5ec148ae537caf50d75825d0f58ba19a
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-21 18:22:41 -0500

    commit

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using MyMath;
+
 namespace MyMath.Tests;
 
 public class Tests
@@ -10,6 +13,15 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        // Arrange
+            int a = 5;
+            int b = 7;
+            int expectedSum = a + b;
+
+            // Act
+            int actualSum = Operations.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedSum, actualSum);
     }
 }
