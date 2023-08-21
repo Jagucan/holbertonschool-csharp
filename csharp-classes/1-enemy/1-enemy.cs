b2604c8e4e67595cd725d858c90508bebf651fa6
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-21 10:02:49 -0500

    task 2

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class represents a Zombie type enemy.
+    /// </summary>
+    public class Zombie
+    {
+
+        public int health;
+
+        /// <summary>Initializes a new instance of the Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+    
+}
