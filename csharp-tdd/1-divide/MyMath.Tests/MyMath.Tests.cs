0910a6a0c721863cfaf0dc16928d56e0daf40469
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-22 18:02:03 -0500

    task 1

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using MyMath;
 
 namespace MyMath.Tests
 {
@@ -12,7 +13,44 @@ namespace MyMath.Tests
         [Test]
         public void TestDivide()
         {
-            Assert.Pass();
+            int[,] matrix1 = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            int[,] result1;
+            result1 = Matrix.Divide(matrix1, 0);
+            
+            int[,] matrix2 = new int[,]
+            {
+                { 2, 4, 6 },
+                { 9, 12, 15 },
+                { -1, -2, -3 }
+            };
+            int[,] result2;
+            result2 = Matrix.Divide(matrix2, 5);
+
+            int[,] matrix3 = new int[,]
+            {
+                { 0, 0 },
+                { 0, 0 },
+                { 0, 0 }
+            };
+
+            int[,] result3;
+            result3 = Matrix.Divide(matrix1, 0);
+
+            int[,] matrix4 = new int[,]
+            {
+                { -1, -2, -3 },
+                { -4, -5, -6 },
+                { -7, -8, -9 }
+            };
+
+            int[,] result4;
+            result4 = Matrix.Divide(matrix1, 10);
         }
     }
 }
