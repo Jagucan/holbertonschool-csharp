3eecbd556c6d593f627ae53c7c77e71842225d93
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-08-22 17:42:35 -0500

    task 1

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDivide()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Matrix
+    {
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            int[,] result = new int[rows, columns];
+
+            try
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        result[i, j] = matrix[i, j] / num;
+                    }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Divide by zero.");
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
