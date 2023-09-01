364790f93ed463e7f0532f673febc3cfdc70b91d
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-01 18:10:07 -0500

    commit

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -5,14 +5,30 @@ public class Obj
 {
     public static void Print(object myObj)
     {
-        Type objectType = myObj.GetType();
-        
-        Console.WriteLine($"Properties and Methods of {objectType.Name}:");
-
-        foreach (MemberInfo member in objectType.GetMembers())
+        Type int32Type = typeof(Int32);
+        Console.WriteLine($"{int32Type.Name} Properties:");
+        Console.WriteLine($"{int32Type.Name} Methods:");
+        foreach (MemberInfo member in int32Type.GetMembers())
         {
             Console.WriteLine(member.Name);
         }
+
+        Type objectType = myObj.GetType();
+        Console.WriteLine($"{objectType.Name} Properties:");
+        foreach (MemberInfo member in objectType.GetMembers())
+        {
+            if (member is PropertyInfo property && property.PropertyType == typeof(int))
+            {
+                Console.WriteLine(property.Name);
+            }
+        }
+        Console.WriteLine($"{objectType.Name} Methods:");
+        foreach (MemberInfo member in objectType.GetMembers())
+        {
+            if (member is MethodInfo method && method.ReturnType == typeof(int))
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
     }
 }
-
---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shape
+{
+    public class Shape
+    {
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+}
