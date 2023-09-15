1da36d6c1b62a860ccdcdde166d6de90d7c9ca3f
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-15 10:55:16 -0500

    task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int dimension = vector.Length;
+
+        if (dimension == 2 || dimension == 3)
+        {
+            double sumOfSquares = 0.0;
+
+            foreach (double component in vector)
+            {
+                sumOfSquares += component * component;
+            }
+
+            double magnitude = Math.Sqrt(sumOfSquares);
+            magnitude = Math.Round(magnitude, 2);
+            
+            return magnitude;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
