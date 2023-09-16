06f0f5f6c8777c47147ddb95b87c7885488e33d4
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-16 13:50:56 -0500

    task 9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        int dimension = vector.Length;
+
+        if (dimension != 2 || dimension != 3)
+        {
+            double[] result = new double[1];
+            result[0] = -1;
+            return result;
+        }
+        else
+        {
+            double[] result = new double[dimension];
+            for (int i = 0; i < dimension; i++)
+            {
+                result[i] = vector[i] * scalar;
+            }
+            return result;
+        }
+    }
+}
