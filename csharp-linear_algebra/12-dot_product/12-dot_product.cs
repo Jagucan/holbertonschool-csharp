89619320f292fb39c02181df4a0f28591b79809b
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-17 16:55:12 -0500

    task 12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12_dot_product
+{
+    class VectorMath
+    {
+        public static double DotProduct(double[] vector1, double[] vector2)
+        {
+            int dimensionV1 = vector1.Length;
+            int dimensionV2 = vector2.Length;
+            
+            if ((dimensionV1 != 2 && dimensionV2 != 3) || dimensionV1 != dimensionV2)
+            {
+                double[] result = new double[1];
+                result[0] = -1;
+                return result;
+            }
+
+            else
+            {
+                double[] productVectors = new double[dimensionV1];
+                double result = 0;
+                for (int i = 0; i < dimensionV1; i++)
+                {
+                    productVectors[i] = vector1[i] * vector2[i];
+                    result += productVectors[i];
+                }
+                return result;
+            }
+        }
+    }
+}
