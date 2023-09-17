99479e5e04a45c5e9b4b74d9043e78098519fecb
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-17 18:38:46 -0500

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != 2 && rows != 3)
+        {
+            double[,] result = new double[1, 1];
+            result[0, 0] = -1;
+            return result;
+        }
+        else
+        {
+            double[,] result = new double[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = matrix[i, j] + scalar;
+                }
+            }
+            return result;
+        }
+    }
+}
