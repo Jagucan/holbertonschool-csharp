f8dc37d476fb2e7b31a11d35f51ba651b4418eca
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-11-09 18:25:29 -0500

    task 3

---
+++ csharp-generics/3-peek/queue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public Queue()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = head;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            T dequeuedNode = head.value;
+            head = head.next;
+            count--;
+            return dequeuedNode;
+        }
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else 
+        { 
+            return head.value; 
+        }
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
