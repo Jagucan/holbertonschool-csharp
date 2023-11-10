a75b86422ab0d20e69a3742f76d96e285cce7928
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-11-09 19:04:38 -0500

    task 4

---
+++ csharp-generics/4-print/queue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public Queue()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = head;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            T dequeuedNode = head.value;
+            head = head.next;
+            count--;
+            return dequeuedNode;
+        }
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            return head.value;
+        }
+    }
+
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.next;
+            }
+        }
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
