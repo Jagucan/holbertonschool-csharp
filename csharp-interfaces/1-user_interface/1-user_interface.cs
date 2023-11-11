5bb0915b79e089397d7537fd4e9e5df3c7aa1f51
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-11-11 13:24:44 -0500

    task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name = string.Empty;
+
+    public override string ToString()
+    {
+        if (name == null)
+        {
+            return "is a " + GetType().Name;
+        }
+        else
+        {
+            return name + " is a " + GetType().Name;
+        }
+
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public void Interact()
+    {
+        // Interact Implemnetation 
+    }
+
+    public int durability { get; set; }
+
+    public void Break()
+    {
+        // Break Implementation 
+    }
+
+    public bool isCollected { get; set; }
+
+    public void Collect()
+    {
+        // Collect Implementation
+    }
+}
