27573c7c7ece453618328c03f726bf8aae5a4608
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-11-11 13:53:27 -0500

    task 2

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -2,57 +2,57 @@
 
 abstract class Base
 {
-    public string name { get; set; }
+	public string name { get; set; }
 
-    public override string ToString()
-    {
-        if (name == null)
-        {
-            return "is a " + GetType().Name;
-        }
-        else
-        {
-            return name + " is a " + GetType().Name;
-        }
+	public override string ToString()
+	{
+		if (name == null)
+		{
+			return "is a " + GetType().Name;
+		}
+		else
+		{
+			return name + " is a " + GetType().Name;
+		}
 
-    }
+	}
 }
 
 interface IInteractive
 {
-    void Interact();
+	void Interact();
 }
 
 interface IBreakable
 {
-    int durability { get; set; }
-    void Break();
+	int durability { get; set; }
+	void Break();
 }
 
 interface ICollectable
 {
-    bool isCollected { get; set; }
-    void Collect();
+	bool isCollected { get; set; }
+	void Collect();
 }
 
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    public void Interact()
-    {
-        // Interact Implemnetation 
-    }
+	public void Interact()
+	{
+		// Interact Implemnetation 
+	}
 
-    public int durability { get; set; }
+	public int durability { get; set; }
 
-    public void Break()
-    {
-        // Break Implementation 
-    }
+	public void Break()
+	{
+		// Break Implementation 
+	}
 
-    public bool isCollected { get; set; }
+	public bool isCollected { get; set; }
 
-    public void Collect()
-    {
-        // Collect Implementation
-    }
+	public void Collect()
+	{
+		// Collect Implementation
+	}
 }
---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        if (name == null)
+        {
+            return "is a " + GetType().Name;
+        }
+        else
+        {
+            return name + " is a " + GetType().Name;
+        }
+
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string name)
+    {
+        if (name == null)
+        {
+            this.name = "Door";
+        }
+        else
+        {
+            this.name = name;
+        }
+    }
+
+    public void Interact()
+    {
+        // Interact Implementation
+        Console.WriteLine("You try to open the " + name + ". It's locked.");
+    }
+}
